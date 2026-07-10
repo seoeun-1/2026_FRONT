0951598d5e4f84740bd6e65bc91ddc9fb786ca72
admin <gmgd055@gmail.com>
2026-07-10 17:09:56 +0900

    day05

---
+++ practice/js_practice1.js
@@ -0,0 +1,52 @@
+//문제1
+//손님에게 이름과 원하는 음료, 그리고 수량을 순서대로 물어본 후, 모든 정보를 조합하여 바리스타가 볼 수 있도록 콘솔에 주문 내역을 출력하는 프로그램을 만드세요.
+
+//요구사항
+//1. prompt()를 사용하여 사용자로부터 다음 정보를 입력받습니다.
+//   손님의 이름,원하는 음료,주문 수량
+//2. let 또는 const를 사용해 입력받은 값들을 각각의 변수에 저장합니다.
+//3. console.log()를 사용하여 정해진 형식으로 주문 내역을 콘솔에 출력합니다.
+
+let name = prompt("이름")
+let 메뉴 = prompt("음료")
+let count = prompt("수량")
+
+console.log("[주문 접수]")
+console.log("이름")
+console.log("name")
+
+console.log("메뉴")
+console.log("menu")
+
+console.log("수량")
+console.log("count")
+
+//문제2
+//사용자에게 이름과 오늘 한 운동 종류, 그리고 운동 시간(분) 을 순서대로 물어본 후, 입력된 정보를 바탕으로 콘솔에 오늘의 운동 기록을 보기 좋게 출력하는 프로그램을 만드세요.
+
+//요구사항
+//1. prompt()를 사용하여 다음 정보를 입력받습니다.
+//사용자의 이름
+//운동 종류
+//운동 시간 (분 단위)
+//2. let 또는 const를 사용해 입력받은 값들을 각각의 변수에 저장합니다.
+//3. console.log()를 사용하여 정해진 형식으로 운동 기록을 콘솔에 출력합니다.
+//4. 분당 소모 칼로리(예: 1분당 7칼로리)를 곱해서 총 소모 칼로리를 계산하고 콘솔에 함께 출력해 보세요.
+
+let name = prompt("사용자의 이름")
+let exercise = prompt("운동종류")
+let time = prompt("운동시간 (분 단위)")
+let calorie = time * 7
+
+console.log("[운동 기록]")
+console.log("이름")
+console.log("name")
+
+console.log("운동종류")
+console.log("exercise")
+
+console.log("운동시간")
+console.log("time")
+
+console.log("칼로리")
+console.log("calorie")
