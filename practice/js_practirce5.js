0cb47c07000dadbd4658b1ae2351444eebb74172
admin <gmgd055@gmail.com>
2026-07-15 17:05:11 +0900

    css_practice.css

--- practice/js_practirce5.js
+++ practice/js_practirce5.js
@@ -4,7 +4,7 @@ prompt를 세 번 사용하여 사용자로부터 '제품명', '가격', '제조
 let 제품명 = prompt('제품명')
 let 가격 = prompt('가격')
 let 제조사 = prompt('제조사')
-const product = 
+const product = { 제품명 : 제품명, 가격 : 가격, 제조사 : 제조사}
 console.log(product)
 
 /*문제 2: prompt로 회원 가입 및 아이디 중복 확인 기능 구현
@@ -12,7 +12,9 @@ prompt를 세 번 사용하여 사용자로부터 '아이디', '비밀번호', '
 입력받은 정보로 하나의 member 객체를 생성하고, members 배열에 저장하여 배열을 콘솔에 출력하시오.
 단] 입력받은 아이디가 이미 배열에 존재하면 '존재하는 아이디 입니다'를 출력하고 배열에 등록하지 않습니다. */
 const members = [{ id: 'user1', password: 'pass1', name: '사용자1' }, { id: 'user2', password: 'pass2', name: '사용자2' },];
-
+let 아이디 = prompt('아이디')
+let 비밀번호 = prompt('비밀번호')
+let 이름 = prompt('이름')
 /*문제 3: 객체 배열의 속성 값 평균 구하기
 scores 배열에 담긴 모든 학생의 수학(math) 점수 평균을 계산하여 콘솔에 출력하시오. */
 const scores = [{ name: 'A', math: 80, science: 92 },{ name: 'B', math: 95, science: 88 },{ name: 'C', math: 76, science: 78 }];
